1f4ac811adb01d70c7ff3ee0ee8e8e6869a566c1
Haris-HH <haris_are@outlook.com>
2025-12-30 18:35:27 +0700

    use new OSM map url

--- src/hooks/useOpenStreetMap.tsx
+++ src/hooks/useOpenStreetMap.tsx
@@ -6,11 +6,7 @@ import html2canvas from 'html2canvas';
 import { MapConfig } from "../features/types";
 import { DEFAULT_MAP_CONFIG } from '../constants/map';
 
-// Config
-import { getUrls } from '../config/runtimeConfig';
-
 export const useMap = (config: Partial<MapConfig> = {}) => {
-  const { VITE_OSM_API_KEY } = getUrls();
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
   const mapInstance = useRef<LeafletMap | null>(null);
@@ -33,10 +29,10 @@ export const useMap = (config: Partial<MapConfig> = {}) => {
         config.zoom ?? DEFAULT_MAP_CONFIG.zoom
       );
 
-      L.tileLayer(`https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}.png?api_key=${VITE_OSM_API_KEY}`, {
-        attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>',
+      L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>',
         maxZoom: 19,
-        crossOrigin: true,
       }).addTo(map);
 
       if (config.zoomControl ? config.zoomControl : DEFAULT_MAP_CONFIG.zoomControl) {
